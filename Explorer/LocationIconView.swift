1bc5b0e26f8fd6c9a1d628981e8d04873a9a3b62
Jack Devey <jack@jw3.uk>
2023-06-19 13:37:36 +0100

    hgfhf

---
+++ Explorer/LocationIconView.swift
@@ -0,0 +1,18 @@
+//
+//  LocationIconView.swift
+//  Explorer
+//
+//  Created by Jack Devey on 19/06/2023.
+//
+
+import SwiftUI
+
+struct LocationIconView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LocationIconView()
+}
---
+++ Explorer/LocationListView.swift
@@ -0,0 +1,18 @@
+//
+//  LocationListView.swift
+//  Explorer
+//
+//  Created by Jack Devey on 19/06/2023.
+//
+
+import SwiftUI
+
+struct LocationListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LocationListView()
+}
